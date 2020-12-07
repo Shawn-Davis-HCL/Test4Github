69220c93b85a2f5268fc1d7613402eeb92525ef8
Shawn Davis <shawn.davis@hcl.com>
2020-12-07 11:34:21 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test4Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
